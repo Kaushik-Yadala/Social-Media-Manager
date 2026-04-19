052d4219d628892b2d71bafd4415f8cc2cd44e04
Srinivas Kolla <kolla.srinag@gmail.com>
2026-04-19 10:22:05 +0000

    feat: implement vector store

--- ml-service/src/find_models.py
+++ ml-service/src/find_models.py
@@ -5,5 +5,5 @@ client = genai.Client(api_key='AIzaSyBwTr5M6MMS-z9bsTL4_thMpbwK6g2JRnE')
 print("Models supporting content generation:")
 for m in client.models.list():
     # Check if 'generateContent' is in the supported_actions list
-    if 'generateContent' in m.supported_actions:
-        print(f"Model ID: {m.name} | Display Name: {m.display_name}")
+    # if 'generateContent' in m.supported_actions:
+    print(f"Model ID: {m.name} | Display Name: {m.display_name}")
